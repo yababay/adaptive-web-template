eb5753ee803429e7fa5c57d740402d1255d53753
mabel <bigbelk@live.ru>
2023-01-15 09:36:29 +0300

    publish

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('@yababay67/postcss-bootstrap-icons'),
+    require('postcss-bootstrap-icons'),
     require('autoprefixer'),
     require('tailwindcss')
   ]
--- src/main.js
+++ src/main.js
@@ -1,2 +1,2 @@
 import './style.css'
-import '@yababay67/postcss-bootstrap-icons/bootstrap-icons-demo.css'
+import './bootstrap-icons.css'
