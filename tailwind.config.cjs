04ee40f1e97ee71b886e54d3041c60aeb75cdfe2
mabel <bigbelk@live.ru>
2023-01-17 08:44:38 +0300

    publish

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '420px',
+      },
+    },
   },
   plugins: [],
 }
